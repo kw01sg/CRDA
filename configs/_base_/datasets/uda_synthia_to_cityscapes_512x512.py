296d55527729f056b53ab466fc6992eddc5257f1
Kian Boon Koh <kianboonkog@gmail.com>
2022-02-25 10:23:11 +0800

    update test.sh and config

--- configs/_base_/datasets/uda_synthia_to_cityscapes_512x512.py
+++ configs/_base_/datasets/uda_synthia_to_cityscapes_512x512.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'CityscapesDataset'
-data_root = 'data/cityscapes/'
+data_root = '/data/kianboonkoh/cityscapes/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 crop_size = (512, 512)
@@ -52,25 +52,25 @@ data = dict(
         type='UDADataset',
         source=dict(
             type='SynthiaDataset',
-            data_root='data/synthia/',
+            data_root='/data/kianboonkoh/synthia/RAND_CITYSCAPES/',
             img_dir='RGB',
             ann_dir='GT/LABELS',
             pipeline=synthia_train_pipeline),
         target=dict(
             type='CityscapesDataset',
-            data_root='data/cityscapes/',
+            data_root='/data/kianboonkoh/cityscapes/',
             img_dir='leftImg8bit/train',
             ann_dir='gtFine/train',
             pipeline=cityscapes_train_pipeline)),
     val=dict(
         type='CityscapesDataset',
-        data_root='data/cityscapes/',
+        data_root='/data/kianboonkoh/cityscapes/',
         img_dir='leftImg8bit/val',
         ann_dir='gtFine/val',
         pipeline=test_pipeline),
     test=dict(
         type='CityscapesDataset',
-        data_root='data/cityscapes/',
+        data_root='/data/kianboonkoh/cityscapes/',
         img_dir='leftImg8bit/val',
         ann_dir='gtFine/val',
         pipeline=test_pipeline))
--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TEST_ROOT=$1
-CONFIG_FILE="${TEST_ROOT}/*${TEST_ROOT: -1}.json"
+CONFIG_FILE="${TEST_ROOT}/${TEST_ROOT##*/}.json"
 CHECKPOINT_FILE="${TEST_ROOT}/latest.pth"
 SHOW_DIR="${TEST_ROOT}/preds/"
 echo 'Config File:' $CONFIG_FILE
